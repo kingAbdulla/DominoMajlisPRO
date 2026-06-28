6db14853ba397fb5672dc2b48f80306581177ac0
kingAbdulla <107007878+kingAbdulla@users.noreply.github.com>
2026-06-28 11:07:24 +0300

    Expose motion command properties on Filament view

--- DominoMajlisPRO/LivingVisualPlatform/Rendering/FilamentLivingVisualView.cs
+++ DominoMajlisPRO/LivingVisualPlatform/Rendering/FilamentLivingVisualView.cs
@@ -8,6 +8,12 @@ public sealed class FilamentLivingVisualView : View
     public static readonly BindableProperty IsPausedProperty =
         BindableProperty.Create(nameof(IsPaused), typeof(bool), typeof(FilamentLivingVisualView), true);
 
+    public static readonly BindableProperty LastMotionCommandProperty =
+        BindableProperty.Create(nameof(LastMotionCommand), typeof(string), typeof(FilamentLivingVisualView), string.Empty);
+
+    public static readonly BindableProperty LastMotionCommandVersionProperty =
+        BindableProperty.Create(nameof(LastMotionCommandVersion), typeof(int), typeof(FilamentLivingVisualView), 0);
+
     public string AssetPath
     {
         get => (string)GetValue(AssetPathProperty);
@@ -18,5 +24,17 @@ public sealed class FilamentLivingVisualView : View
     {
         get => (bool)GetValue(IsPausedProperty);
         set => SetValue(IsPausedProperty, value);
+    }
+
+    public string LastMotionCommand
+    {
+        get => (string)GetValue(LastMotionCommandProperty);
+        set => SetValue(LastMotionCommandProperty, value);
+    }
+
+    public int LastMotionCommandVersion
+    {
+        get => (int)GetValue(LastMotionCommandVersionProperty);
+        set => SetValue(LastMotionCommandVersionProperty, value);
     }
 }
