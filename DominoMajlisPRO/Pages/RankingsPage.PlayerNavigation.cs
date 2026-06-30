e6e28fce2edb6d6b78c5706be4da95ab01b8a1ff
kingAbdulla <107007878+kingAbdulla@users.noreply.github.com>
2026-06-30 14:02:49 +0300

    Remove duplicate player rankings navigation handler

--- DominoMajlisPRO/Pages/RankingsPage.PlayerNavigation.cs
+++
@@ -1,9 +0,0 @@
-namespace DominoMajlisPRO.Pages;
-
-public partial class RankingsPage
-{
-    async void OnPlayersRankingClicked(object? sender, TappedEventArgs e)
-    {
-        await Navigation.PushAsync(new PlayerRankingsPage());
-    }
-}
