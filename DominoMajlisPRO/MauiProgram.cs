efca58a7a65e0779fb9eb7e2460f2ca59660dbc2
kingAbdulla <107007878+kingAbdulla@users.noreply.github.com>
2026-07-11 22:30:55 +0300

    Register cloud services in MAUI startup

--- DominoMajlisPRO/MauiProgram.cs
+++ DominoMajlisPRO/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using DominoMajlisPRO.Cloud;
+using Microsoft.Extensions.Logging;
+
 namespace DominoMajlisPRO
 {
     public static class MauiProgram
@@ -12,7 +14,7 @@ namespace DominoMajlisPRO
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                    fonts.AddFont("BAUHS93.TTF","BAUHS93");
+                    fonts.AddFont("BAUHS93.TTF", "BAUHS93");
                     fonts.AddFont("CinzelDecorative-Bold.ttf", "CinzelDecorative-Bold");
                     fonts.AddFont("HARLOWSI.TTF", "HARLOWSI");
                     fonts.AddFont("Tajawal-Regular.ttf", "Tajawal-Regular");
@@ -22,8 +24,18 @@ namespace DominoMajlisPRO
                     fonts.AddFont("DG-Nemr-V.0.ttf", "DG-Nemr-V.0");
                 });
 
+            var cloudOptions = new CloudApiOptions();
+            builder.Services.AddSingleton(cloudOptions);
+            builder.Services.AddSingleton<CloudSessionStore>();
+            builder.Services.AddHttpClient<CloudApiClient>(client =>
+            {
+                client.BaseAddress = new Uri(cloudOptions.BaseUrl, UriKind.Absolute);
+                client.Timeout = cloudOptions.Timeout;
+                client.DefaultRequestHeaders.UserAgent.ParseAdd("DominoMajlisPRO/1.0");
+            });
+
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
