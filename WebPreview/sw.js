e2bd9b9553d3688d141be8ab218f4828933cbd4f
kingAbdulla <107007878+kingAbdulla@users.noreply.github.com>
2026-07-11 21:34:32 +0300

    Refresh web preview cache for Cloudflare API

--- WebPreview/sw.js
+++ WebPreview/sw.js
@@ -1,5 +1,8 @@
-const CACHE='dmp-web-preview-v1';
+const CACHE='dmp-web-preview-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
-self.addEventListener('fetch',event=>event.respondWith(fetch(event.request).catch(()=>caches.match(event.request).then(response=>response||caches.match('./index.html')))));
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
+self.addEventListener('activate',event=>{event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))]))});
+self.addEventListener('fetch',event=>{
+  if(event.request.url.includes('/api/')) return;
+  event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(response=>response||caches.match('./index.html'))));
+});
