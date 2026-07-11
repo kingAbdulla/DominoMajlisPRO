1e27aebceedb9073d16f9e927372c89b4160b50c
kingAbdulla <107007878+kingAbdulla@users.noreply.github.com>
2026-07-12 00:19:00 +0300

    Refresh web app cache for new shell

--- WebPreview/sw.js
+++ WebPreview/sw.js
@@ -1,8 +1,8 @@
-const CACHE='dmp-web-preview-v2';
+const CACHE='dmp-web-app-v3';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))]))});
 self.addEventListener('fetch',event=>{
   if(event.request.url.includes('/api/')) return;
-  event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(response=>response||caches.match('./index.html'))));
+  event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(response=>response||caches.match('./index.html'))));
 });
